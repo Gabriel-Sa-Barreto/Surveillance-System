b5212015b6b701ccda2badb328b1c4336942cd55
Gabriel-Sa-Barreto <bielbarretoalves@gmail.com>
2025-05-02 17:27:05 -0300

    Implementação de interface para os GPIOs

--- Camera/src/Software_HM_Control/include/src/gpio.c
+++ Camera/src/Software_HM_Control/include/src/gpio.c
@@ -1,6 +1,6 @@
-#include "hearders/gpio.c"
+#include "../gpio.h"
 
-struct gpiod_line_request *gpio_request_lines(const char *chip_path, unsigned int *offset, int num_lines, const char *consumer, char *in_out)
+struct gpiod_line_request* gpio_request_to_lines(const char *chip_path, unsigned int *offset, int num_lines, const char *consumer, char *in_out)
 {
     struct gpiod_request_config *req_cfg  = NULL;
 	struct gpiod_line_request   *request  = NULL;
@@ -12,15 +12,19 @@ struct gpiod_line_request *gpio_request_lines(const char *chip_path, unsigned in
     chip = gpiod_chip_open(chip_path);
 	if (!chip)
 		return NULL;
+    else
+        printf("%s Chip opened successfully.\n", TAG);
     
     settings = gpiod_line_settings_new();
         if (!settings)
             goto close_chip;
+        else
+            printf("%s Line setting created successfully.\n", TAG);
     
-    if(strcmp(int_out, GPIO_DIRECTION_IN) == 0)
+    if(strcmp(in_out, GPIO_DIRECTION_IN) == 0)
     {
         gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
-    }else if(strcmp(int_out, GPIO_DIRECTION_OUT) == 0)
+    }else if(strcmp(in_out, GPIO_DIRECTION_OUT) == 0)
     {
         gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_OUTPUT);
     }else
@@ -29,18 +33,24 @@ struct gpiod_line_request *gpio_request_lines(const char *chip_path, unsigned in
     line_cfg = gpiod_line_config_new();
 	if (!line_cfg)
 		goto free_settings;
+    else
+        printf("%s Line Config successfully.\n", TAG);
     
     for(int i = 0; i < num_lines; i++)
     {
         ret = gpiod_line_config_add_line_settings(line_cfg, &offset[i], 1, settings);
-        if(ret = -1)
+        if(ret)
             goto free_line_config;
+        else
+            printf("%s Line settings added successfully.\n", TAG);
     }
 
     if(consumer) {
 		req_cfg = gpiod_request_config_new();
 		if (!req_cfg)
 			goto free_line_config;
+        else
+            printf("%s Request config consumer successfully.\n", TAG);
 
 		gpiod_request_config_set_consumer(req_cfg, consumer);
 	}
@@ -61,7 +71,7 @@ close_chip:
 	return request;
 }
 
-int gpio_read(gpiod_line_request *request, const unsigned int offset)
+int gpio_read(struct gpiod_line_request *request, const unsigned int offset)
 {
     enum gpiod_line_value value = gpiod_line_request_get_value(request, offset);
     if(value == GPIOD_LINE_VALUE_ACTIVE)
@@ -73,12 +83,12 @@ int gpio_read(gpiod_line_request *request, const unsigned int offset)
     return -1;
 }
 
-void gpio_write(gpiod_line_request *request, const unsigned int offset, enum gpiod_line_value value)
+void gpio_write(struct gpiod_line_request *request, const unsigned int offset, enum gpiod_line_value value)
 {
     gpiod_line_request_set_value(request, offset, value);
 }
 
-void gpio_release_request(gpiod_line_request *request)
+void gpio_release_request(struct gpiod_line_request *request)
 {
     gpiod_line_request_release(request);
 }
--- Camera/src/Software_HM_Control/src/app_main.c
+++ Camera/src/Software_HM_Control/src/app_main.c
@@ -1,31 +1,34 @@
-#include "headers/gpio.h"
-#include <errno.h>
+#include "../include/gpio.h"
+#include <unistd.h>
+#include <stdlib.h>
 
-const unsigned int LED_1_LINE 17;
-const unsigned int LED_2_LINE 27;
-const unsigned int BT_1_LINE 22;
-
-int main(void)
+int main()
 {
-    const char *chipPath;
-    struct gpiod_line_request request_leds;
-    struct gpiod_line_request request_bt;
+    const char *chipPath = "/dev/gpiochip0";
+    struct gpiod_line_request *request_leds;
+    struct gpiod_line_request *request_bt;
+    enum gpiod_line_value value  = GPIOD_LINE_VALUE_ACTIVE;
+    int BT_value = GPIOD_LINE_VALUE_ACTIVE;
 
-    const unsigned int offset_leds[] = { LED_1_LINE, LED_2_LINE };
-    const unsigned int offset_bt[]   = { BT_1_LINE };
+    unsigned int LED_1_LINE = 17;
+    unsigned int LED_2_LINE = 27;
+    unsigned int BT_1_LINE  = 22;
 
-    request_leds = gpio_request_lines(chip_path, offset_leds, "set-leds", GPIO_DIRECTION_OUT);
-    request_bt   = gpio_request_lines(chip_path, offset_bt,   "set-bt", GPIO_DIRECTION_IN);
+    unsigned int offset_leds[2] = { LED_1_LINE, LED_2_LINE };
+    unsigned int offset_bt[1]   = { BT_1_LINE };
+
+    request_leds = gpio_request_to_lines(chipPath, offset_leds, 2, "set-leds", GPIO_DIRECTION_OUT);
+    request_bt   = gpio_request_to_lines(chipPath, offset_bt,   1, "set-bt", GPIO_DIRECTION_IN);
 
     if(!request_leds)
     {
-        fprintf(stderr, "%s Failed to request line to Leds: %s\n", TAG, strerror(errno));
+        printf("%s Failed to request line to Leds:\n", TAG);
         return EXIT_FAILURE;
     }
 
     if(!request_bt)
     {
-        fprintf(stderr, "%s Failed to request line to Butttons: %s\n", TAG, strerror(errno));
+        printf("%s Failed to request line to Butttons: %s\n", TAG);
         return EXIT_FAILURE;
     }
 
@@ -33,6 +36,10 @@ int main(void)
     {
         value = (value == GPIOD_LINE_VALUE_ACTIVE) ? GPIOD_LINE_VALUE_INACTIVE : GPIOD_LINE_VALUE_ACTIVE;
         gpio_write(request_leds, LED_1_LINE, value);
+        gpio_write(request_leds, LED_2_LINE, value);
+
+        BT_value = gpio_read(request_bt, offset_bt[0]);
+        printf("%s Button value: %d\n", TAG, BT_value);
         sleep(1);
     }
 
